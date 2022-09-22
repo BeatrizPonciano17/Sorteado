b705e39bd0d3ab0620c01550ac5e1a6c8070ed08
Bia <88545709+BeatrizPonciano17@users.noreply.github.com>
2022-09-22 14:50:48 -0300

    ajustes

---
+++ src/ProdutoDao.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+import model.PessoaFisica;
+import model.Produto;
+
+public class ProdutoDao {
+
+    public ArrayList<Produto> convertCsvConta(ArrayList<PessoaFisica> listaPessoa) {
+        return null;
+    }
+
+}
